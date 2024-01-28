49977ea6b184289438d0a58ececcc4d65aadd78f
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-01-28 01:08:54 -0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,52 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import Box from "@mui/material/Box";
+import { Typography, Link } from "@mui/material";
 import "./Footer.css";
 import Email from "../assets/icons/email.png";
 
 const Footer: React.FC = () => {
   return (
-    <div id="footer">
-      <Container>
-        <h2>Need Help?</h2>
-        <Row>
-          <Col md className="mb-6">
-            <h3 className="font-semibold">Email us</h3>
+    <Box sx={{ background: "#D92D27", padding: "1.6rem 3rem", color: "white" }}>
+      <Typography variant="h2">Need Help?</Typography>
+      <div
+        className="flex flex-wrap w-full justify-between mb-6"
+        id="footerContentContainer"
+      >
+        <div className="my-6 flex-auto">
+          <Typography variant="h3">Email us</Typography>
+          <div className="flex">
             <img
               src={Email}
               alt="Link to Instagram"
               width={20}
               style={{ marginRight: "0.25em" }}
             />
-            <a
-              style={{ color: "white", textDecoration: "none" }}
+            <Link
+              color="inherit"
               href="mailto:info@senecahackathon.com"
+              className="no-underline"
             >
               info@senecahackathon.com
-            </a>
-          </Col>
-          <Col md className="mb-6">
-            <h3 className="font-semibold">Address</h3>
-            <p style={{ margin: 0 }}>
-              Seneca Polytechnic <br />
-              1750 Finch Ave East
-              <br />
-              Toronto, Ontario, Canada <br />
-              M2J 2X5
-            </p>
-          </Col>
-        </Row>
-        <p style={{ fontSize: "x-small" }}>
-          &copy; Copyright. All rights reserved by Housing Hackathon.
-        </p>
-      </Container>
-    </div>
+            </Link>
+          </div>
+        </div>
+        <div className="my-6 flex-auto">
+          <Typography variant="h3">Address</Typography>
+          <p className="my-1 text-lg">
+            Seneca Polytechnic
+            <br />
+            1750 Finch Ave East
+            <br />
+            Toronto, Ontario, Canada
+            <br />
+            M2J 2X5
+          </p>
+        </div>
+      </div>
+      <p className="text-[x-small]">
+        Ⓒ Copyright. All rights reserved by Housing Hackathon.
+      </p>
+    </Box>
   );
 };
 
