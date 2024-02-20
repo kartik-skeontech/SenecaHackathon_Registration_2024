c52ea3899a9d92481fc6f5c1553d9799260087bc
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-02-19 19:14:23 -0500

    Add Email Triggered from Firebase

--- src/model/data/firebase/index.ts
+++ src/model/data/firebase/index.ts
@@ -16,6 +16,97 @@ import {
   IRegistrationForm,
   CollegeList,
 } from "../../../interface/type";
+import Facebook from "../../../assets/icons/Facebook.png";
+import Instagram from "../../../assets/icons/Instagram.png";
+import LinkedIn from "../../../assets/icons/LinkedIn.png";
+//import TikTok from "../../../assets/icons/TikTok.png";
+import XTwitter from "../../../assets/icons/XTwitter.png";
+
+export async function sendRegistrationEmail(participant: IRegistrationForm) {
+  console.log("participant", participant.email);
+  const emailHtmlContent = `
+    <h3>Dear ${participant.firstName},</h3>
+    <p>Thank you for your interest in Housing Hackathon 2024.</p>
+    <p>This email serves as an automated confirmation that you have been successfully registered to participate in the Qualifier round of the Hackathon, which is scheduled to begin on March 10, 2024.</p>
+    <p>We are thrilled that you've chosen to be a part of this Hackathon, where innovative solutions will be developed to create a positive impact to tackle the Housing challenges. Your participation is invaluable, and we look forward to witnessing the creativity and expertise you bring to the event.</p>
+    <p>Within 2 business days, our student success team will be reaching out to you with detailed information on the next steps of the competition.</p>
+    <p>Should you have any questions or require further assistance, please do not hesitate to contact us via email. Additionally, feel free to connect with us through the social media links provided below.</p>
+    <p>Thank you once again for your participation and we look forward to seeing you at the event.</p>
+    <p>Best regards,</p>
+    <p>Housing Hackathon 2024 Team</p>
+    <hr/>
+    <p>Connect with us:</p>
+<table>
+  <tr>
+    <td align="center" style="padding: 0 10px;">
+    <a
+    href="https://www.instagram.com/senecahackathon/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="icon"
+  >
+    <img src=${Instagram} alt="Instagram" />
+  </a>
+    </td>
+    <td align="center" style="padding: 0 10px;">
+    <a
+    href="https://www.facebook.com/SenecaHackathon2022"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="icon"
+  >
+    <img src=${Facebook} alt="Facebook" />
+  </a>
+    </td>
+    <td align="center" style="padding: 0 10px;">
+    <a
+    href="https://www.linkedin.com/company/seneca-hackathon/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="icon"
+  >
+    <img src=${LinkedIn} alt="LinkedIn" />
+  </a>
+    </td>
+    <td align="center" style="padding: 0 10px;">
+    <a
+    href="https://twitter.com/SenecaHackathon"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="icon"
+  >
+    <img src=${XTwitter} alt="XTwitter" />
+  </a>
+    </td>
+    <td align="center" style="padding: 0 10px;">
+    <a
+    href="https://www.tiktok.com/@senecahackathon"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="icon"
+  >
+    <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" alt="TikTok" />
+  </a>
+    </td>
+  </tr>
+</table>
+    
+    `;
+  try {
+    const isSend = await addDoc(collection(db, "mail"), {
+      to: participant.email,
+      message: {
+        subject: "Registration Confirmation for Housing Hackathon 2024",
+        html: emailHtmlContent,
+      },
+    });
+    if (isSend) {
+      return true;
+    }
+  } catch (error) {
+    console.error("Error sending registration email: ", error);
+  }
+}
 
 export const isEmailExist = async (email: string) => {
   try {
